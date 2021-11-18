0a3c74fb78641d40e2135ca273435e6e8bfd846f
canaelgvin <canaelgvin@gmail.com>
2021-11-18 11:28:45 +0100

    histogram

--- python/histogram.py
+++ python/histogram.py
@@ -6,17 +6,20 @@ samples_T1_unord = np.loadtxt("../out/data/energy_T1.0_unordered_problem5.txt")
 samples_T24_unord = np.loadtxt("../out/data/energy_T2.4_unordered_problem5.txt")
 samples_T24_ord = np.loadtxt("../out/data/energy_T2.4_ordered_problem5.txt")
 
-n_bins = 20
+n_bins = 100
+x_plot_min = 0.0
+x_plot_max = 50.0
+bin_width = (x_plot_max - x_plot_min) / float(n_bins)
 
-lowT_ord = np.histogram(samples_T1_ord)
-lowT_unord = np.histogram(samples_T1_unord)
+lowT_ord = np.histogram(samples_T1_ord, bins = n_bins, density = 1, range = (x_plot_min, x_plot_max))
+lowT_unord = np.histogram(samples_T1_unord, bins = n_bins, density = 1)
 
-highT_ord = np.histogram(samples_T24_ord, bins = n_bins)
-highT_unord = np.histogram(samples_T24_unord)
+highT_ord = np.histogram(samples_T24_ord, bins = n_bins, density = 1)
+highT_unord = np.histogram(samples_T24_unord, bins = n_bins, density = 1)
 
-plt.hist(lowT_unord)
-plt.hist(lowT_unord)
-plt.hist(highT_ord)
-plt.hist(highT_unord)
+plt.hist(lowT_ord)
+# plt.hist(lowT_unord)
+# plt.hist(highT_ord)
+# plt.hist(highT_unord)
 plt.show()
-plt.savefig("../out/plot/5_histogram.pdf")
+plt.savefig("../out/plot/histogram_lowT_ord.pdf")
